4934cff269b3de57d9cee68b7931049d841e59e9
Diego Pino <pinowsky@gmail.com>
2010-12-31 16:28:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiogugu::Application.config.secret_token = 'b7d25753b7e067c28faacbfabbaa3f4837db5ec4f693ffe4a07f2ae98349e77cd133fb5a86eeb8b1be7020f014f9af0c71a5d161b4f9c7faefd16ed2a4f8c93d'
