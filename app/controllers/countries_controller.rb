e6c9bf7730fff73e1be540fb14f00915690ca037
Diego Pino <pinowsky@gmail.com>
2011-01-24 23:19:45 +0100

    Show radios sorted by location for each country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -8,8 +8,8 @@ class CountriesController < ApplicationController
         _show(country)
         return
       end
-      countries_and_continents = Location.select("distinct(country),continent").order("continent");
-      @countries_by_continent = to_countries_by_continent(countries_and_continents);
+      countries_and_continents = Location.select("distinct(country),continent").order("continent")
+      @countries_by_continent = to_countries_by_continent(countries_and_continents)
 
       respond_to do |format|
           format.html # { render :action => "countries" }
@@ -18,12 +18,23 @@ class CountriesController < ApplicationController
   end
 
   def _show(country)
-    @locations = Location.where("country = ?", country)
+    @locations = Location.select("id, location").where("country = ?", country).order("location")
+	@radios_by_location = to_radios_by_location(@locations)
 
     respond_to do |format|
         format.html { render :action => "locations" }
         format.xml  { render :xml => @countries.errors, :status => :unprocessable_entity }
     end
+  end
+
+  def to_radios_by_location(locations)
+	result = Hash.new
+	locations.each {|location|
+		radios = Radio.where("location_id = ?", location[:id])
+		city = location[:location]
+		result[city] = radios
+	}
+	return result.sort
   end
 
   # GET /locations/countries
@@ -35,10 +46,6 @@ class CountriesController < ApplicationController
         format.html # show.html.erb
         format.xml  { render :xml => @country }
     end
-  end
-
-  def cities_by_country(locations)
-
   end
 
   def to_countries_by_continent(countries_and_continents)
