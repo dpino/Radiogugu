6f368e420518b537ae874eb3b68989da3a501f86
Diego Pino <pinowsky@gmail.com>
2011-01-25 01:00:02 +0100

    Show location breadcrumb in radio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+DOUBLE_ARROW = " >> "
+
+def location_breadcrumb(location_id)
+    location = Location.find(location_id)
+    return location[:continent] + DOUBLE_ARROW + location[:country] + DOUBLE_ARROW + location[:location]
+end
+
 end
