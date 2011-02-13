8954ea040572cbf590c37b8e98394ffc1e05ef07
Diego Pino <pinowsky@gmail.com>
2011-02-13 02:21:16 +0100

    Make parts in breadcrumb linkable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
+
   def index
+    @popular_stations = Radio.limit(5)
+    @popular_countries = ["Spain", "Japan", "China", "Italy", "Canada"]
+    @ads = Radio.limit(5)
+
+    respond_to do |format|
+      format.html # { render :action => "home" }
+    end
+
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,18 @@ DOUBLE_ARROW = " >> "
 
 def location_breadcrumb(location_id)
     location = Location.find(location_id)
-    return location[:continent] + DOUBLE_ARROW + location[:country] + DOUBLE_ARROW + location[:location]
+
+    continent = location[:continent]
+    continent_url = link_to continent, url_for(:controller => "countries", :order => "continent") + "##{continent}"
+
+    country = location[:country]
+    country_url = link_to country, url_for(:controller => "countries", :id => country)
+
+    location = location[:location]
+    # FIXME: The generated URL depdens on the sort order, need to store it in the session
+    location_url = link_to location, url_for(:controller => "countries", :order => "location", :id=> country) + "#" + location
+
+    return continent_url + DOUBLE_ARROW + country_url + DOUBLE_ARROW + location_url
 end
 
 end
