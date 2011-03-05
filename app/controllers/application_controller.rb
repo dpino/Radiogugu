df2df7dcee818846fec8e138bf04870d1ec8c23a
Diego Pino <pinowsky@gmail.com>
2011-03-05 19:01:24 +0100

    Add back_to_url in Device
    FIXME: redirect is kind of cumbersome, need to register in session
    'back_to_url' parameter and overwrite a method in application_controller
    that handles the redirection. But, at this moment, I don't know how to
    capture the back_to_url parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
     return ActionView::Base.new.distance_of_time_in_words(time, Time.now)
   end
 
+  def after_sign_in_path_for(resource)
+    puts "after_sign_in_path_for"
+    if request.session[:return_to].is_a? String
+      [request.session[:return_to], request.session[:return_params].to_query].join("?")
+    elsif request.session[:return_to].is_a? Hash
+      request.session[:return_to].merge!(request.session[:return_params])
+    else
+      super
+    end
+  end
+
 end
