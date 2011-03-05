40d2c68332d1837bf2ac64f17ea241825796c683
Diego Pino <pinowsky@gmail.com>
2011-03-05 20:25:16 +0100

    Show location in list of radio stations by country

--- app/models/radio.rb
+++ app/models/radio.rb
@@ -1,8 +1,8 @@
 class Radio < ActiveRecord::Base
-    acts_as_rateable
-    validates :name, :presence => true
-    validates :url, :presence => true
+  acts_as_rateable
+  validates :name, :presence => true
+  validates :url, :presence => true
 
-    belongs_to :location
+  belongs_to :location
   has_many :comments, :order => "updated_at desc"
 end
